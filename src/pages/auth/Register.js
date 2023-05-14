b58785a6ee480f276bcff98fdf8b4f73790499e4
Danijel Vilovski <danivilovski@gmail.com>
2023-05-14 22:31:38 +0200

    Add initial Order and User Address on Register

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import './Register.css'
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import jwtDecode from 'jwt-decode'
 
 export default function Register() {
 
@@ -10,6 +12,7 @@ export default function Register() {
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const navigate = useNavigate();
+    var token = "";
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -30,6 +33,8 @@ export default function Register() {
         .then((response) => response.json())
         .then((json) => {
             if(json.status === 200) {
+                token = jwtDecode(json.transferObject)
+                setOrderAndAddress();
                 navigate('/login');
             } else if (json.status === 409) {
                 document.getElementById('register-username-span').innerHTML = json.message;        
@@ -40,6 +45,68 @@ export default function Register() {
             }
         })
         
+    }
+
+    const setOrderAndAddress = () => {
+        axios.post('https://localhost:7079/UserAddress/addUserAddress', {
+            userId: Number(token.UserId),
+            address: null,
+            city: null,
+            postalCode: null,
+            country: null,
+            phone: null
+        })
+        .then(response => {
+            if(response.status === 200) {
+                axios.get(`https://localhost:7079/UserAddress/useraddress/${Number(token.UserId)}`)
+                .then(response => {
+                    axios.post('https://localhost:7079/Order/addOrder', {
+                        userId: Number(token.UserId),
+                        orderDate: '2023-05-14',
+                        userAddressId: response.data.transferObject.id,
+                        shippingMethodId: 1,
+                        orderStatusId: 1,
+                        orderTotal: 0
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+            } 
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+
+        // axios.get(`https://localhost:7079/UserAddress/useraddress/${Number(token.UserId)}`)
+        // .then(response => {
+        //     console.log(response.data)
+        //     setAddedAddress(response.data)
+        // })
+        // .catch(err => {
+        //     console.log(err)
+        // })
+
+
+
+        // axios.post('https://localhost:7079/Order/addOrder', {
+        //     userId: Number(token.UserId),
+        //     orderDate: '2023-05-14',
+        //     userAddressId: addedAddress.transferObject.id,
+        //     shippingMethodId: 1,
+        //     orderStatusId: 1,
+        //     orderTotal: 0
+        // })
+        // .then(function (response) {
+        //     console.log(response);
+        // })
+        // .catch(function (error) {
+        //     console.log(error);
+        // });
     }
 
     return (
